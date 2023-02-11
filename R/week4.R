9a7e79263c0729b4f0ec9ddef9ca1d5863559208
Lina Huang <huan2384@umn.edu>
2023-02-10 18:42:49 -0600

    reorganize qs column

--- R/week4.R
+++ R/week4.R
@@ -5,3 +5,5 @@ library(tidyverse)
 #Data Import
 import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum","parnum","stimver","datadate","qs"))
 glimpse(import_tbl)
+wide_tbl <-separate(import_tbl, qs, c("q1", "q2", "q3", "q4", "q5"), sep = "-")
+wide_tbl[c("q1", "q2", "q3", "q4", "q5")] <- sapply(wide_tbl[c("q1", "q2", "q3", "q4", "q5")], as.integer)
