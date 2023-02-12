52c23ed5819447ccbddcae91d32cff948f41edfc
Lina Huang <huan2384@umn.edu>
2023-02-12 09:22:14 -0600

    update line 11& remove Q2 missing values

--- R/week4.R
+++ R/week4.R
@@ -8,4 +8,13 @@ glimpse(import_tbl)
 wide_tbl <-separate(import_tbl, qs, c("q1", "q2", "q3", "q4", "q5"), sep = "-")
 wide_tbl[c("q1", "q2", "q3", "q4", "q5")] <- sapply(wide_tbl[c("q1", "q2", "q3", "q4", "q5")], as.integer)
 wide_tbl$datadate<- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
-wide_tbl[,c("q1", "q2", "q3", "q4", "q5")][wide_tbl[,c("q1", "q2", "q3", "q4", "q5")] == 0] <- NA
+is.na(wide_tbl[,c("q1", "q2", "q3", "q4", "q5")]) <- !wide_tbl[,c("q1", "q2", "q3", "q4", "q5")]
+
+
+
+
+wide_tbl<- wide_tbl[!is.na(wide_tbl$q2), ]
+
+
+
+
