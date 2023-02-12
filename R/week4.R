7525bceaf7d91bcf83b697413f3fe00116595892
Lina Huang <huan2384@umn.edu>
2023-02-11 18:34:40 -0600

    change datetimes and missing value

--- R/week4.R
+++ R/week4.R
@@ -7,3 +7,5 @@ import_tbl <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenu
 glimpse(import_tbl)
 wide_tbl <-separate(import_tbl, qs, c("q1", "q2", "q3", "q4", "q5"), sep = "-")
 wide_tbl[c("q1", "q2", "q3", "q4", "q5")] <- sapply(wide_tbl[c("q1", "q2", "q3", "q4", "q5")], as.integer)
+wide_tbl$datadate<- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S")
+wide_tbl[,c("q1", "q2", "q3", "q4", "q5")][wide_tbl[,c("q1", "q2", "q3", "q4", "q5")] == 0] <- NA
